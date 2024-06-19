417e2b7f126b6835115b71daaaf2506a56b5ab19
tasfia236 <tasfiatabassum300@gmail.com>
2024-06-20 03:02:29 +0600

    Profile

---
+++ src/pages/Dashboard/Profile/Form.jsx
@@ -0,0 +1,88 @@
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+
+const Form = ({ profile, refetch }) => {
+    const axiosSecure = useAxiosPublic();
+
+    const updateProfile = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const image = form.photoURL.value;
+        const oldpass = form.oldpass.value;
+        const newpass = form.newpass.value;
+
+        const updateProfile = { name, email, image, newpass };
+        console.log(updateProfile);
+        console.log(oldpass, profile[0]?.password);
+
+        if (oldpass === profile[0]?.password) {
+            axiosSecure.patch(`/users/tourGuide/${profile[0]?._id}`, updateProfile)
+                .then(res => {
+                    console.log(res.data);
+                    if (res.data.modifiedCount > 0) {
+                        refetch();
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: `Updated ${profile[0]?.name}'s Profile!`,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "Old password is incorrect",
+            });
+        }
+    };
+
+    return (
+        <div className='p-8'>
+            <h1 className="text-center text-3xl font-black text-blue-500">{profile[0]?.role} Information Form</h1>
+            <div className="card lg:card-side shadow-2xl bg-base-100 my-12">
+                <form onSubmit={updateProfile} className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" id="name" name="name" defaultValue={profile[0]?.name} className="w-full px-3 py-2 border rounded-md" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" id="email" name="email" defaultValue={profile[0]?.email} className="w-full px-3 py-2 border rounded-md" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Image URL</span>
+                        </label>
+                        <input type="text" id="image" name="photoURL" defaultValue={profile[0]?.image} className="w-full px-3 py-2 border rounded-md" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Old Password</span>
+                        </label>
+                        <input type="password" id="oldpass" name="oldpass" className="w-full px-3 py-2 border rounded-md" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">New Password</span>
+                        </label>
+                        <input type="password" id="newpass" name="newpass" defaultValue={profile[0].password} className="w-full px-3 py-2 border rounded-md" />
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Form;
--- src/pages/Dashboard/Profile/Profile.jsx
+++ src/pages/Dashboard/Profile/Profile.jsx
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Form from "./Form";
 
 const Profile = () => {
 
     const axiosSecure = useAxiosSecure();
     const { user } = useAuth();
     console.log(user?.email)
-    const { data: profile = [] } = useQuery({
+    const { data: profile = [], refetch } = useQuery({
         queryKey: ['profile', user?.email],
         queryFn: async () => {
             const res = await axiosSecure.get(`/user?email=${user.email}`);
@@ -20,16 +21,16 @@ const Profile = () => {
     return (
         <div className="p-8">
             <h1 className="text-center text-3xl font-black text-blue-800">My Profile</h1>
-            <div className=" flex justify-between px-16 pr-96 py-12 w-full">
+            <div className=" flex justify-between gap-10 px-16 pr-24 py-12 w-full">
                 <div className=" w-40 ml-20">
                     <img src={profile[0]?.image} className=" rounded-btn" alt="" />
                 </div>
                 <div  className="py-12 space-y-12">
-                    <h1 className="text-xl"><span className="font-bold">{profile[0].role} Name:</span>  {profile[0].name}</h1>
-                    <h1 className="text-xl"><span className="font-bold">{profile[0].role} Email:</span>  {profile[0].email}</h1>
+                    <h1 className="text-xl"><span className="font-bold">{profile[0]?.role} Name:</span>  {profile[0]?.name}</h1>
+                    <h1 className="text-xl"><span className="font-bold">{profile[0]?.role} Email:</span>  {profile[0]?.email}</h1>
                 </div>
             </div>
-            
+            <Form profile={profile} refetch={refetch}></Form>
         </div>
     );
 };
